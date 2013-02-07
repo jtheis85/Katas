5131aeb1b5ca5e37d4e03104b7154670442838cd
jtheis85 <jatheis2@gmail.com>
2013-02-06 21:20:11 -0600

    Maximum subarray problem - TDD
    Arrived at something like Kadane's algorithm

---
+++ ContiguousSum/ContiguousSum/ContiguousSum.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighestContiguousSum
+{
+  /// <summary>
+  /// 
+  /// </summary>
+  public static class Calculator
+  {
+    /// <summary>
+    /// Computes the highest contiguous sum within an array of integers.
+    /// A contiguous sum is integers next to each other in the array.
+    /// </summary>
+    /// <example>
+    /// For the array [4, 0, -5, 6, 5], the answer should be 11, because
+    /// 6 and 5 are next to each other, and adding -5 would reduce the sum.
+    /// </example>
+    /// <param name="inputArray">An array of integers to sum</param>
+    /// <returns>The sum, or null if the array was empty</returns>
+    public static int? HighestContiguousSum(int[] inputArray)
+    {
+      // Validate input
+      if (inputArray.Length == 0) { return null; }
+
+      else
+      {
+        int currentSum = inputArray[0];
+        int bestSum = inputArray[0];
+
+        //Starting at the 2nd element
+        for (int i = 1; i < inputArray.Length; i++)
+        {
+          int value = inputArray[i];
+          // Take a better value if we have a negative
+          if (bestSum < 0 && bestSum < value)
+          {
+            bestSum = value;
+            currentSum = value;
+          }
+          else if (value > 0 || (value < 0 && value > -1 * currentSum))
+          {
+            currentSum += value;
+            bestSum = Math.Max(currentSum, bestSum);
+          }
+          else if (value <= -1 * currentSum)
+          {
+            currentSum = 0;
+          }
+        }
+
+        return bestSum;
+      }
+    }
+  }
+}
---
+++ ContiguousSum/ContiguousSumUnitTests/Calculator_HighestContiguousSumTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using HighestContiguousSum;
+
+namespace HighestContiguousSumUnitTests
+{
+  [TestClass]
+  public class Calculator_HighestContiguousSumTests
+  {
+    [TestMethod]
+    public void HighestContiguousSum_OfEmptyArray_ReturnsNull()
+    {
+      var inputArray = new int[0];
+      Calculator.HighestContiguousSum(inputArray).Should().Be(null);
+    }
+
+    [TestMethod]
+    public void HighestContiguousSum_OfSingleElementArray_ReturnsElement()
+    {
+      var inputArray = new int[1];
+      Calculator.HighestContiguousSum(inputArray).Should().Be(inputArray[0]);
+    }
+
+    [TestMethod]
+    public void HighestContigiousSum_OfNegativeAndPositive_ReturnsPositive()
+    {
+      var inputArray = new int[] { -1, 1 };
+      Calculator.HighestContiguousSum(inputArray).Should().Be(1);
+    }
+
+    [TestMethod]
+    public void HighestContiguousSum_OfNegativeValues_ReturnsLeastNegativeValue()
+    {
+      var inputArray = new int[] { -3, -2 };
+      Calculator.HighestContiguousSum(inputArray).Should().Be(-2);
+    }
+
+    [TestMethod]
+    public void HighestContiguousSum_OfNonNegativeValues_ReturnsSumOfValues()
+    {
+      var inputArray = new int[] { 1, 2, 0, 4};
+      Calculator.HighestContiguousSum(inputArray).Should().Be(7);
+    }
+
+    [TestMethod]
+    public void HighestContiguousSum_OfNegativeSplit_ReturnsBestSide()
+    {
+      var inputArray = new int[] { 2, -3, 3 };
+      Calculator.HighestContiguousSum(inputArray).Should().Be(3);
+    }
+
+    [TestMethod]
+    public void HighestContiguousSum_OfIncludedNegative_ReturnsSumWithNegative()
+    {
+      var inputArray = new int[] { 2, -1, 2 };
+      Calculator.HighestContiguousSum(inputArray).Should().Be(3);
+    }
+
+    [TestMethod]
+    public void HighestContiguousSum_OfThreeSections_ReturnsBestSection()
+    {
+      var inputArray = new int[] { 5, -2, 1, -4, 4 };
+      Calculator.HighestContiguousSum(inputArray).Should().Be(5);
+    }
+
+    [TestMethod]
+    public void HighestContiguousSum_WithMultipleNegatives_ReturnsSumWithoutNegatives()
+    {
+      var inputArray = new int[] { -1, -1, 1, 2, -1, -1, -1, 2, -1, -1, -1};
+      Calculator.HighestContiguousSum(inputArray).Should().Be(3);
+    }
+
+    [TestMethod]
+    public void HighestContiguousSum_AllNegative_ReturnsTheLeastNegative()
+    {
+      var inputArray = new int[] { -8, -3, -4, -1, -2, -1, -5, -7 };
+      Calculator.HighestContiguousSum(inputArray).Should().Be(-1);
+    }
+
+    [TestMethod]
+    public void HighestContiguousSum_CommunitySubmission_1()
+    {
+      var inputArray = new int[] { -2, -3, 4, -1, -2, 1, 5, -3 };
+      Calculator.HighestContiguousSum(inputArray).Should().Be(7);
+    }
+  }
+}
