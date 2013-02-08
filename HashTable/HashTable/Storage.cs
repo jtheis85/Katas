e30260fbce74781fc5ea00fb13c269d2dacbc291
jtheis85 <jatheis2@gmail.com>
2013-02-07 20:45:45 -0600

    Hashtable Exploration - TDD
    A quick exploration of Hashtables. Turns out, these are essentially
    obsolete with the invention of generic Dictionaries, which are strongly
    typed, and avoid the performance penalties of casting, as well as boxing
    and unboxing value types.

---
+++ HashTable/HashTable/Storage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableKata
+{
+    public class Storage
+    {
+      private Hashtable hash = new Hashtable();
+
+      public void Store(int key, object item)
+      {
+        hash.Add(key, item);
+      }
+
+      public object Retrieve(int key)
+      {
+        return hash[key];
+      }
+
+      public bool Has(params object[] items)
+      {
+        bool hasAllItems = true;
+
+        foreach (var item in items)
+        {
+          hasAllItems &= hash.ContainsValue(item);
+        }
+
+        return hasAllItems;
+      }
+
+      public bool FunctionalHas(IEnumerable<object> items)
+      {
+        return items.All(item => hash.ContainsValue(item));
+      }
+
+      public string GetString(int key)
+      {
+        return hash[key] as string;
+      }
+    }
+}
---
+++ HashTable/HashTableUnitTests/StorageTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using System.Collections.Generic;
+
+namespace HashTableKata
+{
+  [TestClass]
+  public class StorageTests
+  {
+    [TestMethod]
+    public void Store_Null_ReturnsNull()
+    {
+      var storage = new Storage();
+      storage.Store(1, null);
+      storage.Retrieve(1).Should().Be(null);
+    }
+
+    [TestMethod]
+    public void Store_Int_ReturnsInt()
+    {
+      var storage = new Storage();
+      storage.Store(1, 1);
+      storage.Retrieve(1).Should().Be(1);
+    }
+
+    [TestMethod]
+    public void Store_AnonymousType_ReturnsAnonymousType()
+    {
+      var storage = new Storage();
+      var item = new { name = "Starbuck", rank = "Lieutennant", serialNumber = 1337 };
+      storage.Store(1, item);
+      storage.Retrieve(1).Should().Be(item);
+    }
+
+    [TestMethod]
+    public void Has_MultipleItems_ReturnsTrue()
+    {
+      var storage = new Storage();
+      storage.Store(0, 1);
+      storage.Store(1, 2);
+      storage.Store(2, 3);
+
+      storage.Has(1,2,3).Should().Be(true);
+      storage.FunctionalHas(new List<object>() { 1, 2, 3 }).Should().Be(true);
+    }
+
+    [TestMethod]
+    public void Has_MultipleItems_ReturnsFalse()
+    {
+      var storage = new Storage();
+      storage.Store(0, 1);
+      storage.Store(1, 2);
+      storage.Store(2, 3);
+
+      storage.Has(1, 4, 3).Should().Be(false);
+    }
+
+    [TestMethod]
+    public void GetString_GivenString_ReturnString()
+    {
+      var storage = new Storage();
+      storage.Store(0, "My Item");
+
+      storage.GetString(0).GetType().Should().Be(typeof(string));
+      storage.GetString(0).Should().Be("My Item");
+    }
+
+    [TestMethod]
+    public void GetString_GivenInt_ReturnsNull()
+    {
+      var storage = new Storage();
+      storage.Store(0, 1);
+
+      storage.GetString(0).Should().Be(null);
+    }
+  }
+}
