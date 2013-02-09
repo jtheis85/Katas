b26744e483028c3b20d02ddff53e9efede9e6dcf
jtheis85 <jatheis2@gmail.com>
2013-02-09 11:14:37 -0600

    Extension Method Kata
    Exploring extension methods using generics for array operations

---
+++ ExtensionMethodKata/ExtensionMethodKata/ArrayExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodKata
+{
+    public static class ArrayExtension
+    {
+      public static T[] Concat<T>(this T[] first, T[] second)
+      {
+        int oldLength = first.Length;
+        Array.Resize(ref first, first.Length + second.Length);
+        Array.Copy(second, 0, first, oldLength, second.Length);
+
+        return first;
+      }
+    }
+}
---
+++ ExtensionMethodKata/ExtensionMethodTests/ArrayExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace ExtensionMethodKata
+{
+  [TestClass]
+  public class ArrayExtensionsTests
+  {
+    [TestMethod]
+    public void ConcatTest()
+    {
+      var first = new int[] { 1, 2, 3, 4 };
+      var second = new int[] { 5, 6, 7, 8 };
+
+      first.Concat(second).Should().ContainInOrder(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 });
+    }
+  }
+}
