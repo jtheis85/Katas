373f7ef6a00ae2dbe022c566e39d4aef5df6b990
jtheis85 <jatheis2@gmail.com>
2013-02-08 23:58:44 -0600

    Quicksort Kata
    A probably naive implementation of Quicksort, also trying to force
    myself to use arrays.

---
+++ ArrayQuicksortKata/ArrayQuicksortKata/Sorter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayQuicksortKata
+{
+    public static class Sorter
+    {
+      public static int[] Quicksort(int[] input)
+      {
+        if (input.Length < 2)
+        {
+          return input;
+        }
+
+        int pivot = input[input.Length / 2];
+        int[] lessThanPivot = input.Where(item => item < pivot).ToArray();
+        int[] equalToPivot = input.Where(item => item == pivot).ToArray();
+        int[] greaterThanPivot = input.Where(item => item > pivot).ToArray();
+
+        int[] result = new int[input.Length];
+
+        Quicksort(lessThanPivot).CopyTo(result,0);
+        equalToPivot.CopyTo(result, lessThanPivot.Length);
+        Quicksort(greaterThanPivot).CopyTo(result, lessThanPivot.Length + equalToPivot.Length);
+
+        return result;
+      }
+    }
+}
---
+++ ArrayQuicksortKata/ArrayQuicksortTests/QuicksortTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace ArrayQuicksortKata
+{
+  [TestClass]
+  public class QuicksortTests
+  {
+    [TestMethod]
+    public void Quicksort_EmptyArray_ReturnsEmptyArray()
+    {
+      var input = new int[0];
+      Sorter.Quicksort(input).Should().BeEmpty();
+    }
+
+    [TestMethod]
+    public void Quicksort_SingleElementArray_ReturnsArray()
+    {
+      var input = new int[] { 1 };
+      Sorter.Quicksort(input).Should().BeInAscendingOrder();
+      Sorter.Quicksort(input).Length.Should().Be(1);
+    }
+
+    [TestMethod]
+    public void Quicksort_TwoElementArray_ReturnsOrderedElements()
+    {
+      var input = new int[] { 2, 1 };
+      Sorter.Quicksort(input).Should().BeInAscendingOrder();
+      Sorter.Quicksort(input).Length.Should().Be(2);
+    }
+
+    [TestMethod]
+    public void Quicksort_ManyElementArray_ReturnsOrderedElements()
+    {
+      var input = new int[] { 2, 1 , 10, 3, 13, 5, 5, 2, 45, 11, 6, 6, 5, 6, 1, 2, 3, 4, 2, 1, 8};
+      Sorter.Quicksort(input).Should().BeInAscendingOrder();
+      Sorter.Quicksort(input).Length.Should().Be(21);
+    }
+  }
+}
