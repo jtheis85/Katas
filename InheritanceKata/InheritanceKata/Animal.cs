e6e2c3352edc9d90ba3fdb38713e230a54b52d08
jtheis85 <jatheis2@gmail.com>
2013-02-08 21:23:22 -0600

    Inheritance Kata
    A kata reviewing inheritance, abstract, virtual, override

---
+++ InheritanceKata/InheritanceKata/Animal.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceKata
+{
+  public abstract class Thing
+  {
+    public abstract string Speak();
+  }
+
+  public class Animal : Thing
+  {
+    public override string Speak()
+    {
+      return "Animal noises";
+    }
+  }
+
+  public class Dog : Animal
+  {
+    public override string Speak()
+    {
+      return "Bark";
+    }
+
+    public virtual string MakeAnimalNoises()
+    {
+      return base.Speak();
+    }
+  }
+
+  public class Chihuahua : Dog
+  {
+    public override string Speak()
+    {
+      return base.Speak() + " Yip yip";
+    }
+
+    public override string MakeAnimalNoises()
+    {
+      return base.MakeAnimalNoises();
+    }
+  }
+
+  public class Robot : Thing
+  {
+    public override string Speak()
+    {
+      return "01011100";
+    }
+  }
+}
---
+++ InheritanceKata/InheritanceTests/AnimalTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InheritanceKata
+{
+  [TestClass]
+  public class AnimalTests
+  {
+    [TestMethod]
+    public void Speak_Animal_AnimalNoises()
+    {
+      var animal = new Animal();
+      animal.Speak().Should().Be("Animal noises");
+    }
+
+    [TestMethod]
+    public void Speak_Dog_Bark()
+    {
+      var dog = new Dog();
+      dog.Speak().Should().Be("Bark");
+      dog.MakeAnimalNoises().Should().Be("Animal noises");
+    }
+
+    [TestMethod]
+    public void Speak_Chihuahua_YipYip()
+    {
+      var chihuahua = new Chihuahua();
+      chihuahua.Speak().Should().Be("Bark Yip yip");
+      chihuahua.MakeAnimalNoises().Should().Be("Animal noises");
+    }
+
+    [TestMethod]
+    public void Speak_Animals_Cacophony()
+    {
+      var animals = new List<Animal>() 
+      {
+        new Animal(),
+        new Dog(),
+        new Chihuahua()
+      };
+
+      animals.Select(animal => animal.Speak()).Should().ContainInOrder(new List<string>() {"Animal noises", "Bark", "Bark Yip yip"});
+    }
+
+    [TestMethod]
+    public void Speak_Robot_Binary()
+    {
+      var robot = new Robot();
+      robot.Speak().Should().Be("01011100");
+    }
+
+    [TestMethod]
+    public void Speak_Things_Cacophony()
+    {
+      var things = new List<Thing>() 
+      {
+        new Animal(),
+        new Dog(),
+        new Chihuahua(),
+        new Robot()
+      };
+
+      things.Select(thing => thing.Speak()).Should().ContainInOrder(new List<string>() { "Animal noises", "Bark", "Bark Yip yip", "01011100" });
+    }
+  }
+}
